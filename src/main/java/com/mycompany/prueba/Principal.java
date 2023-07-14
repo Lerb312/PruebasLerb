c5daedd589f1a6b7be5495df98efa05b16d94e3f
LuisR <luisrodriguezjob0@gmail.com>
2023-07-14 10:58:02 -0400

    Creado proyecto maven prueba

---
+++ src/main/java/com/mycompany/prueba/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author ceos
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Persona p1= new Persona("Luis", 23);
+        System.out.println(p1);
+    }
+}
