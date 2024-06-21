12e7db2d264f4e4d0f3a136591c37d161658cfb0
Lerb312 <138533020+Lerb312@users.noreply.github.com>
2024-06-21 15:43:57 -0400

    bueno

---
+++ AlgoritmosQS/src/main/java/com/lerbqs/algoritmosqs/AlgoritmosQS.java
@@ -0,0 +1,11 @@
+package com.lerbqs.algoritmosqs;
+public class AlgoritmosQS {
+
+    public static void main(String[] args) {
+      // Intro in = new Intro();
+       // in.setVisible(true);
+        
+        Desafio ds = new Desafio();
+        ds.setVisible(true);
+    }
+}
